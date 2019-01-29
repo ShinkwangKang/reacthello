333c30534a09a2dbe228b8daab37f4d7f33880ce
shine1115 <shine1115@gmail.com>
2019-01-29 13:56:23 +0900

    class / className 차이 저장

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 const FunctionalComponent = () => (
   <div>
-    <h1>함수형 컴포넌트</h1>
+    <h1 className="box">함수형 컴포넌트</h1>
     <h2>현재 시간은? {new Date().toLocaleTimeString()}.</h2>
   </div>
 );
@@ -12,7 +12,7 @@ class StatelessComponent extends React.Component {
   render() {
     return (
       <div>
-        <h1>상태 없는 컴포넌트</h1>
+        <h1 className="box2">상태 없는 컴포넌트</h1>
         <h2>현재 시간은? {new Date().toLocaleTimeString()}.</h2>
       </div>
     );
