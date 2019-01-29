2f3997c17fa82dc0f0c02dad804fca011819394d
shine1115 <shine1115@gmail.com>
2019-01-29 13:47:47 +0900

    상태있는/없는 컴포넌트 실습

--- src/index.js
+++ src/index.js
@@ -1,57 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-// 1일차
-/*
-ReactDOM.render(<App />, document.getElementById('root'));
-*/
-/*
-const MyElem = <p> 이렇게 써도됨? </p>;
-const MyCom = () => <p> 이렇게 써도됨? </p>;
+const FunctionalComponent = () => (
+  <div>
+    <h1>함수형 컴포넌트</h1>
+    <h2>현재 시간은? {new Date().toLocaleTimeString()}.</h2>
+  </div>
+);
 
-console.log(MyCom);
-console.log(MyElem, MyCom(), <myCom />);
-
-class MyKlassCom extends React.Component() {
+class StatelessComponent extends React.Component {
   render() {
     return (
-      <di>
-        <p>클래스형 컴포넌트</p>
-        <MyCom />
-      </di>
+      <div>
+        <h1>상태 없는 컴포넌트</h1>
+        <h2>현재 시간은? {new Date().toLocaleTimeString()}.</h2>
+      </div>
     );
   }
 }
-console.log(MyKlassCom);
-console.log(<MyKlassCom />);
-*/
+// 상태있는 컴포넌트
+class StatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      now: new Date().toLocaleTimeString(),
+    };
 
-// 2일차
-// #1
-//const MyElem = <p> 이렇게 써도됨? </p>;
-//const MyCom = () => <p> 이렇게 써도됨? </p>;
-//console.log(MyElem, MyCom());
+    // 되는 코드
+    setTimeout(() => {
+      this.setState({ow: new Date().toLocaleTimeString()});
+    }, 5000);
 
-// #2
-/*
-function tick() {
-  console.log('tick', this);
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+    /*
+    // 안되는 코드
+    setTimeout(function() {
+      console.log(this);
+      this.setState({ow: new Date().toLocaleTimeString()});
+    }, 5000);
+    */
+  }
+  render() {
+    return (
+      <div>
+        <h1>상태 있는 컴포넌트</h1>
+        <h2>현재 시간은? {this.state.now}.</h2>
+      </div>
+    );
+  }
 }
 
-const nTimer = setInterval(tick, 100);
-
-const nTimer2 = setTimeout(function() {
-  clearInterval(nTimer);
-}, 5000);
-console.log(nTimer, nTimer2);
-*/
+ReactDOM.render(
+  <div>
+    <FunctionalComponent />
+    <StatelessComponent />
+    <StatefulComponent />
+  </div>,
+  document.getElementById('root')
+);
