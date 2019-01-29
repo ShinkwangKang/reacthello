077b9958b9012c60e514932c5bebb729384a63f6
shine1115 <shine1115@gmail.com>
2019-01-29 17:31:19 +0900

    실습 예제 2

--- src/component/LectureGoalList.jsx
+++ src/component/LectureGoalList.jsx
@@ -20,4 +20,11 @@ const LectureGoalList = () =>{
 }
 // 외부 참조를 위해 export 한다.
 export default LectureGoalList;
-
+/*
+자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8
+React 개발에 필요한 환경을 구축한다.
+개발편의를 위한 VSCode IDE를 익힌다.
+기본적인 Git사용법을 익힌다.
+PR코드 리뷰를 응용한 개발 프로세스를 익힌다.
+React로 간단한 노트앱을 만들어본다.
+*/
--- src/component/Todos.jsx
+++ src/component/Todos.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 //import 'Todos.css';
 
-// app.js 에서 props 를 배열로 받아서 map로 돌면서 그려보는 예제
+// #2. app.js 에서 props 를 배열로 받아서 map로 돌면서 그려보는 예제
 const Todos = (props) => {
   console.log('====>', props);
   return(
@@ -9,7 +9,7 @@ const Todos = (props) => {
     <h2>{props.title}</h2>
       <ul>
         {props.items.map((item, index) => {
-          return <li key={index}><input type="checkbox"/>{item}</li>;
+          return <li key={index}><input type="checkbox" checked={item.completed}/>{item.name}</li>;
         })}
       </ul>
     </div>
