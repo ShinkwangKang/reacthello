ee5de11e9a1ac88b1c71568d907e8d35d201946d
shine1115 <shine1115@gmail.com>
2019-01-30 14:34:45 +0900

    3일차 Timer 예제.. 부모 자식간 데이터 주고받기

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Todos from './component/Todos';
 import Timer from './component/Timer';
 
 class App extends Component {
+  handleExpiredTimer(arg) {
+    console.log('타이머 종료!!!!!!!!!!!!!!!!!!!!!!!!', arg);
+  }
   render() {
     return (
       <div className="App">
-        <Timer expireDate={'2019-01-30 T18:00:00+09:00'} />;
+        <Timer expireDate={'2019-01-30T14:02:00+09:00'} onExpired={this.handleExpiredTimer} />
       </div>
     );
   }
--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -13,25 +13,51 @@ class Timer extends React.Component {
     };
   }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        mtNow: moment(),
-      });
-    }, 1000);
-  }
-
-  render() {
+  checkExpired = () => {
     const {expireDate} = this.props;
     const {mtNow} = this.state;
     const mtExpire = moment(expireDate);
+    // 비교 후 값 리턴
+    return mtExpire < mtNow;
+  };
 
-    console.log(expireDate, mtNow.format('a hh:mm'));
+  componentDidMount() {
+    console.log('componentDidMount');
+    if (!this.checkExpired()) {
+      this.nTimer = setInterval(() => {
+        this.setState({
+          mtNow: moment(),
+        });
+      }, 1000);
+    }
+  }
+
+  componentDidUpdate() {
+    //console.log('componentDidUpdate');
+    if (this.checkExpired()) {
+      clearInterval(this.nTimer);
+    }
+  }
+
+  render() {
+    const {expireDate, onExpired} = this.props;
+    const {mtNow} = this.state;
+    const mtExpire = moment(expireDate);
+
+    //console.log(expireDate, mtNow.format('a hh:mm'));
+
+    const isExpire = mtExpire < mtNow;
+
+    console.log(onExpired('히히'));
 
     return (
       <div className="Timer">
         <div>현재시간은 {mtNow.format('a hh:mm')}</div>
-        <div>금일 강의 종료까지 {mtExpire.fromNow()}분 남았습니다.</div>
+        {isExpire ? (
+          <div>금일 강의 종료 되었습니다.</div>
+        ) : (
+          <div>금일 강의 종료까지 {mtExpire.fromNow()} 남았습니다.</div>
+        )}
       </div>
     );
   }
